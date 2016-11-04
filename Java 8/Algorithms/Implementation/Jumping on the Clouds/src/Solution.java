b5dbab2bf462e07cefb00dca3b26c5f29a4e599d
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:58:57 +0900

    added Jumping on the Clouds solution

---
+++ Java 8/Algorithms/Implementation/Jumping on the Clouds/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int c[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			c[i] = in.nextInt();
+		}
+		int steps = 0;
+		int location = 0;
+		while (location < n) {
+			if (location == n - 2) {
+				steps++;
+				break;
+			}
+			if (c[location + 2] == 0) {
+				location += 2;
+			} else if (c[location + 1] == 0) {
+				location++;
+			}
+			steps++;
+			if (location == n - 1) {
+				break;
+			}
+		}
+		System.out.println(steps);
+	}
+}
