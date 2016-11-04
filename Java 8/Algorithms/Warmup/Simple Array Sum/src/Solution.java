ddec05f782a84f169a1197a47cfeaa7a484364db
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:10:07 +0900

    simplified solution

--- Java 8/Algorithms/Warmup/Simple Array Sum/src/Solution.java
+++ Java 8/Algorithms/Warmup/Simple Array Sum/src/Solution.java
@@ -1,15 +1,13 @@
-import java.io.BufferedInputStream;
 import java.util.Scanner;
 
 public class Solution {
 
 	public static void main(String[] args) {
-		try (Scanner scanner = new Scanner(new BufferedInputStream(System.in))) {
-			int size = Integer.parseInt(scanner.nextLine());
-			String[] numbers = scanner.nextLine().split(" ");
+		try (Scanner in = new Scanner(System.in)) {
+			int N = in.nextInt();
 			int sum = 0;
-			for (String number : numbers) {
-				sum += Integer.parseInt(number);
+			for (int i = 0; i < N; i++) {
+				sum += in.nextInt();
 			}
 			System.out.println(sum);
 		}
