29de87da3f460339a066f53dff831d8fedce3530
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:19:34 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Service Lane/src/Solution.java
+++ Java 8/Algorithms/Implementation/Service Lane/src/Solution.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		int T = in.nextInt();
-		int[] width = new int[N];
-		for (int i = 0; i < N; i++) {
-			width[i] = in.nextInt();
-		}
-		for (int i = 0; i < T; i++) {
-			int entry = in.nextInt();
-			int exit = in.nextInt();
-			int min = 3;
-			for (int j = entry; j <= exit; j++) {
-				min = (width[j] < min) ? width[j] : min;
+		try (Scanner in = new Scanner(System.in)) {
+			int N = in.nextInt();
+			int T = in.nextInt();
+			int[] width = new int[N];
+			for (int i = 0; i < N; i++) {
+				width[i] = in.nextInt();
 			}
-			System.out.println(min);
+			for (int i = 0; i < T; i++) {
+				int entry = in.nextInt();
+				int exit = in.nextInt();
+				int min = 3;
+				for (int j = entry; j <= exit; j++) {
+					min = (width[j] < min) ? width[j] : min;
+				}
+				System.out.println(min);
+			}
 		}
 	}
 }
