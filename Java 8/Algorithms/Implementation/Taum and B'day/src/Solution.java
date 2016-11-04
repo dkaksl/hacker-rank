3edc828acbe815a14ab2fa06280b2fc19d9c06a5
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:22:13 +0900

    added Taum and B'day solution

---
+++ Java 8/Algorithms/Implementation/Taum and B'day/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++) {
+			long b = in.nextLong();
+			long w = in.nextLong();
+			long x = in.nextLong();
+			long y = in.nextLong();
+			long z = in.nextLong();
+
+			long cost = 0;
+			if (x < (y + z)) {
+				cost += (b * x);
+			} else {
+				cost += ((y + z) * b);
+			}
+			if (y < (x + z)) {
+				cost += (w * y);
+			} else {
+				cost += ((x + z) * w);
+			}
+			System.out.println(cost);
+		}
+	}
+}
