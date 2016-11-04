f711e758533f1c236ad72d271bec55f1b6d12d30
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:45:19 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Kangaroo/src/Solution.java
+++ Java 8/Algorithms/Implementation/Kangaroo/src/Solution.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int x1 = in.nextInt();
-		int v1 = in.nextInt();
-		int x2 = in.nextInt();
-		int v2 = in.nextInt();
-		if (doesCrash(x1, v1, x2, v2)) {
-			System.out.println("YES");
-		} else {
-			System.out.println("NO");
+		try (Scanner in = new Scanner(System.in)) {
+			int x1 = in.nextInt();
+			int v1 = in.nextInt();
+			int x2 = in.nextInt();
+			int v2 = in.nextInt();
+			if (doesCrash(x1, v1, x2, v2)) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
 		}
 	}
 
