97fd48d0f225568a6f2d073caf9037732b554997
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:14:27 +0900

    added Find Digits solution

---
+++ Java 8/Algorithms/Implementation/Find Digits/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for (int i = 0; i < T; i++) {
+			int number = in.nextInt();
+			String numberString = Integer.toString(number);
+			int numberStringLength = numberString.length();
+			int[] digits = new int[numberStringLength];
+			int count = 0;
+			for (int j = 0; j < numberStringLength; j++) {
+				int digit = Character.getNumericValue(numberString.charAt(j));
+				if (digit != 0) {
+					if (number % digit == 0) {
+						count++;
+					}
+				}
+			}
+			System.out.println(count);
+		}
+
+	}
+}
