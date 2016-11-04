c4b18d2aa04c7e02600a994c74910a979de6fd38
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:12:44 +0900

    added Minimum Distances solution

---
+++ Java 8/Algorithms/Implementation/Minimum Distances/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int A[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			A[i] = in.nextInt();
+		}
+		int min = n;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				if (i == j) {
+					continue;
+				}
+				if (A[i] == A[j]) {
+					int newMin = j - i;
+					if (newMin < min) {
+						min = newMin;
+					}
+				}
+			}
+		}
+		if (min == n) {
+			System.out.println(-1);
+		} else {
+			System.out.println(min);
+		}
+	}
+}
