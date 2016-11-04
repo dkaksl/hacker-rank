cd4cdc34b691283e67f760894d6995e5d529290d
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:28:54 +0900

    added Maximizing XOR solution

---
+++ Java 8/Algorithms/Bit Manipulation/Maximizing XOR/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int L = in.nextInt();
+		int R = in.nextInt();
+		int max = 0;
+		for (int i = 0; i <= (R - L); i++) {
+			for (int j = 0; j <= (R - L); j++) {
+				int candidate = ((L + i) ^ (L + j));
+				max = (candidate > max) ? candidate : max;
+			}
+		}
+		System.out.println(max);
+	}
+}
