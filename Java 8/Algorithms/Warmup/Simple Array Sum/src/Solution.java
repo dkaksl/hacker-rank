f956b98c63e9ef8ae2ca1914a91ee8cde4a9a696
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:06:51 +0900

    added solution class

---
+++ Java 8/Algorithms/Warmup/Simple Array Sum/src/Solution.java
@@ -0,0 +1,17 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+		int size = Integer.parseInt(scanner.nextLine());
+		String[] numbers = scanner.nextLine().split(" ");
+		int sum = 0;
+		for (String number : numbers) {
+			sum += Integer.parseInt(number);
+		}
+		System.out.println(sum);
+	}
+
+}
