40dbbe73c349340a3038a49f87629a83d15bcac6
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:24:34 +0900

    added CamelCase solution

---
+++ Java 8/Algorithms/Strings/CamelCase/src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextLine().replaceAll("[A-Z]", " ").split(" ").length);
+	}
+}
