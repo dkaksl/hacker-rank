8cd5e52466d1c5f24e8504b35a4e3900b35a3fbf
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:19:05 +0900

    added Service Lane solution

---
+++ Java 8/Algorithms/Implementation/Service Lane/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int T = in.nextInt();
+		int[] width = new int[N];
+		for (int i = 0; i < N; i++) {
+			width[i] = in.nextInt();
+		}
+		for (int i = 0; i < T; i++) {
+			int entry = in.nextInt();
+			int exit = in.nextInt();
+			int length = exit - entry;
+			int min = 3;
+			for (int j = entry; j <= exit; j++) {
+				min = (width[j] < min) ? width[j] : min;
+			}
+			System.out.println(min);
+		}
+	}
+}
