a5e22d2656223f82ff26f01cc1748ce04ecb681c
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:12:54 +0900

    changed to use try with resources

--- Java 8/Algorithms/Warmup/Compare the Triplets/src/Solution.java
+++ Java 8/Algorithms/Warmup/Compare the Triplets/src/Solution.java
@@ -6,20 +6,21 @@ public class Solution {
 	private static int bobScore = 0;
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int a0 = in.nextInt();
-		int a1 = in.nextInt();
-		int a2 = in.nextInt();
-		int b0 = in.nextInt();
-		int b1 = in.nextInt();
-		int b2 = in.nextInt();
+		try (Scanner in = new Scanner(System.in)) {
+			int a0 = in.nextInt();
+			int a1 = in.nextInt();
+			int a2 = in.nextInt();
+			int b0 = in.nextInt();
+			int b1 = in.nextInt();
+			int b2 = in.nextInt();
 
-		evaluateScores(a0, b0);
-		evaluateScores(a1, b1);
-		evaluateScores(a2, b2);
+			evaluateScores(a0, b0);
+			evaluateScores(a1, b1);
+			evaluateScores(a2, b2);
 
-		System.out
-				.println("$A $B".replace("$A", Integer.toString(aliceScore)).replace("$B", Integer.toString(bobScore)));
+			System.out.println(
+					"$A $B".replace("$A", Integer.toString(aliceScore)).replace("$B", Integer.toString(bobScore)));
+		}
 	}
 
 	private static void evaluateScores(int a, int b) {
