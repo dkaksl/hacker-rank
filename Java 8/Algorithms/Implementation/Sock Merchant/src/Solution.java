683d92a6453a2fc0fddb301a26b6bf4541a22931
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:56:15 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Sock Merchant/src/Solution.java
+++ Java 8/Algorithms/Implementation/Sock Merchant/src/Solution.java
@@ -8,19 +8,20 @@ import java.util.Set;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		Integer c[] = new Integer[n];
-		for (int i = 0; i < n; i++) {
-			c[i] = in.nextInt();
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			Integer c[] = new Integer[n];
+			for (int i = 0; i < n; i++) {
+				c[i] = in.nextInt();
+			}
+			List<Integer> list = Arrays.asList(c);
+			Set<Integer> set = new HashSet<Integer>(list);
+			int count = 0;
+			for (Integer number : set) {
+				count += Collections.frequency(list, number) / 2;
+			}
+			System.out.println(count);
 		}
-		List<Integer> list = Arrays.asList(c);
-		Set<Integer> set = new HashSet<Integer>(list);
-		int count = 0;
-		for (Integer number : set) {
-			count += Collections.frequency(list, number) / 2;
-		}
-		System.out.println(count);
 	}
 
 }
