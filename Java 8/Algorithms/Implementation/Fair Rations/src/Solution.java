3f4b15de3c777518bca9391ac945a9e82549cdae
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:16:43 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Fair Rations/src/Solution.java
+++ Java 8/Algorithms/Implementation/Fair Rations/src/Solution.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		int B[] = new int[N];
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			B[i] = in.nextInt();
-			sum += B[i];
-		}
-
-		if (sum % 2 != 0) {
-			System.out.println("NO");
-		} else {
-			int count = 0;
+		try (Scanner in = new Scanner(System.in)) {
+			int N = in.nextInt();
+			int B[] = new int[N];
+			int sum = 0;
 			for (int i = 0; i < N; i++) {
-				if ((B[i] % 2) != 0) {
-					B[i + 1] = B[i + 1] + 1;
-					count += 2;
-				}
+				B[i] = in.nextInt();
+				sum += B[i];
 			}
-			System.out.println(count);
+
+			if (sum % 2 != 0) {
+				System.out.println("NO");
+			} else {
+				int count = 0;
+				for (int i = 0; i < N; i++) {
+					if ((B[i] % 2) != 0) {
+						B[i + 1] = B[i + 1] + 1;
+						count += 2;
+					}
+				}
+				System.out.println(count);
+			}
 		}
 	}
 }
