bcc011b7bfc3a4bc14bdedfabb1d50ef07488130
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:07:36 +0900

    adapted to use try with resources

--- Java 8/Algorithms/Warmup/Simple Array Sum/src/Solution.java
+++ Java 8/Algorithms/Warmup/Simple Array Sum/src/Solution.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(new BufferedInputStream(System.in));
-		int size = Integer.parseInt(scanner.nextLine());
-		String[] numbers = scanner.nextLine().split(" ");
-		int sum = 0;
-		for (String number : numbers) {
-			sum += Integer.parseInt(number);
+		try (Scanner scanner = new Scanner(new BufferedInputStream(System.in))) {
+			int size = Integer.parseInt(scanner.nextLine());
+			String[] numbers = scanner.nextLine().split(" ");
+			int sum = 0;
+			for (String number : numbers) {
+				sum += Integer.parseInt(number);
+			}
+			System.out.println(sum);
 		}
-		System.out.println(sum);
 	}
 
 }
