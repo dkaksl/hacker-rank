d450dc74d09f5fee3cbecb67cad9dc6c9a075665
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:01:23 +0900

    removed unused code

--- Java 8/Algorithms/Warmup/Solve Me First/src/Solution.java
+++ Java 8/Algorithms/Warmup/Solve Me First/src/Solution.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class Solution {
 
-	static int solveMeFirst(int a, int b) {
-		return a + b;
-	}
-
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in)) {
 			System.out.println(in.nextInt() + in.nextInt());
