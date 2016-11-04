3383a561d6498d1405c55a1dc9d15642a1feeba3
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:11:11 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Jumping on the Clouds - Revisited/src/Solution.java
+++ Java 8/Algorithms/Implementation/Jumping on the Clouds - Revisited/src/Solution.java
@@ -6,18 +6,19 @@ public class Solution {
 	private static int location = 0;
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int k = in.nextInt();
-		int c[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			c[i] = in.nextInt();
-		}
-		jump(c, k, n);
-		while (location != 0) {
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			int k = in.nextInt();
+			int c[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				c[i] = in.nextInt();
+			}
 			jump(c, k, n);
+			while (location != 0) {
+				jump(c, k, n);
+			}
+			System.out.println(E);
 		}
-		System.out.println(E);
 	}
 
 	private static void jump(int[] c, int k, int n) {
