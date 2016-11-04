d968319a44ce83819a82aa62087b5eeeca50b587
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:59:15 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Jumping on the Clouds/src/Solution.java
+++ Java 8/Algorithms/Implementation/Jumping on the Clouds/src/Solution.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int c[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			c[i] = in.nextInt();
-		}
-		int steps = 0;
-		int location = 0;
-		while (location < n) {
-			if (location == n - 2) {
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			int c[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				c[i] = in.nextInt();
+			}
+			int steps = 0;
+			int location = 0;
+			while (location < n) {
+				if (location == n - 2) {
+					steps++;
+					break;
+				}
+				if (c[location + 2] == 0) {
+					location += 2;
+				} else if (c[location + 1] == 0) {
+					location++;
+				}
 				steps++;
-				break;
+				if (location == n - 1) {
+					break;
+				}
 			}
-			if (c[location + 2] == 0) {
-				location += 2;
-			} else if (c[location + 1] == 0) {
-				location++;
-			}
-			steps++;
-			if (location == n - 1) {
-				break;
-			}
+			System.out.println(steps);
 		}
-		System.out.println(steps);
 	}
 }
