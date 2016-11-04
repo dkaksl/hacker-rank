4d0f109f6a017a610ff5ddfe16b2c93fae65269b
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:29:11 +0900

    changed to use try with resources

--- Java 8/Algorithms/Bit Manipulation/Maximizing XOR/src/Solution.java
+++ Java 8/Algorithms/Bit Manipulation/Maximizing XOR/src/Solution.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int L = in.nextInt();
-		int R = in.nextInt();
-		int max = 0;
-		for (int i = 0; i <= (R - L); i++) {
-			for (int j = 0; j <= (R - L); j++) {
-				int candidate = ((L + i) ^ (L + j));
-				max = (candidate > max) ? candidate : max;
+		try (Scanner in = new Scanner(System.in)) {
+			int L = in.nextInt();
+			int R = in.nextInt();
+			int max = 0;
+			for (int i = 0; i <= (R - L); i++) {
+				for (int j = 0; j <= (R - L); j++) {
+					int candidate = ((L + i) ^ (L + j));
+					max = (candidate > max) ? candidate : max;
+				}
 			}
+			System.out.println(max);
 		}
-		System.out.println(max);
 	}
 }
