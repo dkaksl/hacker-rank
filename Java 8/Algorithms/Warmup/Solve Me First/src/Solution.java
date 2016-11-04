f5450a0d9d450da177b4ce83076b0929776812cd
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 16:59:50 +0900

    removed fluff

--- Java 8/Algorithms/Warmup/Solve Me First/src/Solution.java
+++ Java 8/Algorithms/Warmup/Solve Me First/src/Solution.java
@@ -7,13 +7,10 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int a;
-		a = in.nextInt();
-		int b;
-		b = in.nextInt();
-		int sum;
-		sum = solveMeFirst(a, b);
-		System.out.println(sum);
+		try (Scanner in = new Scanner(System.in)) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			System.out.println(a + b);
+		}
 	}
 }
