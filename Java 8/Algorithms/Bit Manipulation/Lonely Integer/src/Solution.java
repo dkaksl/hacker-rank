f606fcec00c915c71d2624191e45dca46f37852a
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:27:45 +0900

    changed to use try with resources, moved methods

--- Java 8/Algorithms/Bit Manipulation/Lonely Integer/src/Solution.java
+++ Java 8/Algorithms/Bit Manipulation/Lonely Integer/src/Solution.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class Solution {
 
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = in.nextInt();
+			}
+			System.out.println(lonelyInteger(a));
+		}
+	}
+
 	private static int lonelyInteger(int[] a) {
 		int lonelyInteger = 0;
 		for (int i = 0; i < a.length; i++) {
 			lonelyInteger ^= a[i];
 		}
 		return lonelyInteger;
-	}
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = in.nextInt();
-		}
-		System.out.println(lonelyInteger(a));
 	}
 
 }
