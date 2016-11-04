f22e03df2ae0250e00ac02be2a8a6ef627aad6dd
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:50:24 +0900

    added Divisible Sum Pairs solution

---
+++ Java 8/Algorithms/Implementation/Divisible Sum Pairs/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int k = in.nextInt();
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = in.nextInt();
+		}
+
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				if (i < j) {
+					if (((a[i] + a[j]) % k) == 0) {
+						count++;
+					}
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
