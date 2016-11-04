650cfb1b70579d2cfa14cd66b687579704a51e06
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:57:56 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Repeated String/src/Solution.java
+++ Java 8/Algorithms/Implementation/Repeated String/src/Solution.java
@@ -3,22 +3,23 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String s = in.next();
-		int sLength = s.length();
-		long n = in.nextLong();
-		int aCount = 0;
-		for (int i = 0; i < sLength; i++) {
-			if (s.charAt(i) == 'a') {
-				aCount++;
+		try (Scanner in = new Scanner(System.in)) {
+			String s = in.next();
+			int sLength = s.length();
+			long n = in.nextLong();
+			int aCount = 0;
+			for (int i = 0; i < sLength; i++) {
+				if (s.charAt(i) == 'a') {
+					aCount++;
+				}
 			}
-		}
-		long count = (n / sLength) * aCount;
-		for (int i = 0; i < (n % sLength); i++) {
-			if (s.charAt(i) == 'a') {
-				count++;
+			long count = (n / sLength) * aCount;
+			for (int i = 0; i < (n % sLength); i++) {
+				if (s.charAt(i) == 'a') {
+					count++;
+				}
 			}
+			System.out.println(count);
 		}
-		System.out.println(count);
 	}
 }
