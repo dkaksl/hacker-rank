3bc45fd01ba9fa935eea6610aadc8d08dfc26d77
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:53:52 +0900

    added Cut the sticks solution

---
+++ Java 8/Algorithms/Implementation/Cut the sticks/src/Solution.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Integer[] sticks = new Integer[in.nextInt()];
+		for (int i = 0; i < sticks.length; i++) {
+			sticks[i] = in.nextInt();
+		}
+		while (sticks.length > 0) {
+			System.out.println(sticks.length);
+			sticks = getCutSticks(sticks);
+		}
+	}
+
+	private static int getSmallestNumber(Integer[] numbers) {
+		int smallest = numbers[0];
+		for (int number : numbers) {
+			smallest = (smallest < number) ? smallest : number;
+		}
+		return smallest;
+	}
+
+	private static Integer[] getCutSticks(Integer[] sticks) {
+		List<Integer> newSticks = new ArrayList<Integer>();
+		int smallest = getSmallestNumber(sticks);
+		for (int i = 0; i < sticks.length; i++) {
+			int cutStick = sticks[i] - smallest;
+			if (cutStick > 0) {
+				newSticks.add(cutStick);
+			}
+		}
+		return newSticks.toArray(new Integer[0]);
+	}
+}
