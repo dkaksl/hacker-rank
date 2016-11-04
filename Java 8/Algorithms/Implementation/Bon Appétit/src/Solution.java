e17748298be88559584667efea3ecd2e5b6d9b61
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:08:27 +0900

    changed to use try with resources

--- Java 8/Algorithms/Implementation/Bon Appétit/src/Solution.java
+++ Java 8/Algorithms/Implementation/Bon Appétit/src/Solution.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int k = in.nextInt();
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = in.nextInt();
-		}
-		int bSum = 0;
-		int bCharged = in.nextInt();
-		for (int i = 0; i < n; i++) {
-			bSum += a[i];
-		}
-		int bActual = (bSum / 2) - (a[k] / 2);
-		if (bCharged == bActual) {
-			System.out.println("Bon Appetit");
-		} else {
-			System.out.println(bCharged - bActual);
+		try (Scanner in = new Scanner(System.in)) {
+			int n = in.nextInt();
+			int k = in.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = in.nextInt();
+			}
+			int bSum = 0;
+			int bCharged = in.nextInt();
+			for (int i = 0; i < n; i++) {
+				bSum += a[i];
+			}
+			int bActual = (bSum / 2) - (a[k] / 2);
+			if (bCharged == bActual) {
+				System.out.println("Bon Appetit");
+			} else {
+				System.out.println(bCharged - bActual);
+			}
 		}
 	}
 }
