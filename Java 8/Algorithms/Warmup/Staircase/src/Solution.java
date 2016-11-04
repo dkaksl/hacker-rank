b4a4ca5575e8310da2ec684d6b35a6fc36cdff26
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:28:18 +0900

    changed to use try with resources

--- Java 8/Algorithms/Warmup/Staircase/src/Solution.java
+++ Java 8/Algorithms/Warmup/Staircase/src/Solution.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int width = scanner.nextInt();
-		for (int i = 0; i < width; i++) {
-			char[] spaces = new char[width - 1 - i];
-			Arrays.fill(spaces, ' ');
-			char[] hashes = new char[i + 1];
-			Arrays.fill(hashes, '#');
-			System.out.println(new String(spaces) + new String(hashes));
+			for (int i = 0; i < width; i++) {
+				char[] spaces = new char[width - 1 - i];
+				Arrays.fill(spaces, ' ');
+				char[] hashes = new char[i + 1];
+				Arrays.fill(hashes, '#');
+				System.out.println(new String(spaces) + new String(hashes));
+			}
 		}
 	}
 }
