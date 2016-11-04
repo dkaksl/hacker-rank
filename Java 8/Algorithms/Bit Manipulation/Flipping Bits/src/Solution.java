16f16e32d4dbc971f02a5c5a874e63a82b83db31
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 18:31:13 +0900

    changed to use try with resources

--- Java 8/Algorithms/Bit Manipulation/Flipping Bits/src/Solution.java
+++ Java 8/Algorithms/Bit Manipulation/Flipping Bits/src/Solution.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		long T = in.nextLong();
-		for (int i = 0; i < T; i++) {
-			System.out.println((Long.MAX_VALUE & 0xFFFFFFFFL) ^ in.nextLong());
+		try (Scanner in = new Scanner(System.in)) {
+			long T = in.nextLong();
+			for (int i = 0; i < T; i++) {
+				System.out.println((Long.MAX_VALUE & 0xFFFFFFFFL) ^ in.nextLong());
+			}
 		}
 	}
 }
