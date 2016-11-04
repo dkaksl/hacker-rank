3f8f86bd0156d8e7e10dcd064025b737ac52ac51
Amani Kilumanga <amani.kilumanga@gmail.com>
2016-11-04 17:48:46 +0900

    added Viral Advertising solution

---
+++ Java 8/Algorithms/Implementation/Viral Advertising/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	private static int likers = 0;
+	private static int advertisees = 5;
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int days = in.nextInt();
+		for (int i = 0; i < days; i++) {
+			addLikers();
+		}
+		System.out.println(likers);
+	}
+
+	private static void addLikers() {
+		int newLikers = advertisees / 2;
+		likers += newLikers;
+		advertisees = (newLikers * 3);
+	}
+}
