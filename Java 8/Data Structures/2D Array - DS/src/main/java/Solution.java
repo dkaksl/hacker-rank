d5777af9604cf2dbed467c8a9a4e4cebdb4828f2
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-10 20:12:51 +0900

    added solution

--- Java 8/Data Structures/2D Array - DS/src/main/java/Solution.java
+++ Java 8/Data Structures/2D Array - DS/src/main/java/Solution.java
@@ -15,7 +15,27 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in)) {
-
+			int[][] A = new int[6][6];
+			for (int y = 0; y < 6; y++) {
+				for (int x = 0; x < 6; x++) {
+					A[y][x] = in.nextInt();
+				}
+			}
+			int maxSum = 0;
+			for (int y = 1; y < 4; y++) {
+				for (int x = 1; x < 4; x++) {
+					int newSum = sumHourGlass(A, x, y);
+					maxSum = (newSum > maxSum) ? newSum : maxSum;
+				}
+			}
+			System.out.println(maxSum);
 		}
+	}
+
+	private static int sumHourGlass(int[][] A, int x, int y) {
+		return //
+		A[x - 1][y - 1] + A[x][y - 1] + A[x + 1][y - 1] + //
+				A[x][y] +//
+				A[x - 1][y + 1] + A[x][y + 1] + A[x + 1][y + 1];
 	}
 }
