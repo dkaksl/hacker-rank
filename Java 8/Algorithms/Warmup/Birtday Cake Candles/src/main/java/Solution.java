3f6765d153f21f5e99280b51384a62c75d33dd01
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-10 19:51:28 +0900

    added solution

---
+++ Java 8/Algorithms/Warmup/Birtday Cake Candles/src/main/java/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+/**
+ * @author Amani
+ *
+ */
+public class Solution {
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			int age = in.nextInt();
+			int maxHeight = 0;
+			int count = 0;
+
+			for (int i = 0; i < age; i++) {
+				int height = in.nextInt();
+				if (height > maxHeight) {
+					count = 1;
+				} else if (height == maxHeight) {
+					count++;
+				} else {
+					continue;
+				}
+			}
+			System.out.println(count);
+		}
+	}
+}
