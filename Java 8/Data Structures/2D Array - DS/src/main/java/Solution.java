a996c6c9e22818ce9253959dfa2d614f96b5cd6c
Amani Kilumanga <amani.kilumanga@gmail.com>
2018-03-10 20:01:23 +0900

    added solution stub

---
+++ Java 8/Data Structures/2D Array - DS/src/main/java/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/** 
+ * 
+ * Copyright (C) 2018 Amani
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+/**
+ * @author Amani
+ *
+ */
+public class Solution {
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+
+		}
+	}
+}
